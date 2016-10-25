1bcbd1ad282a069ad157e046df59392c572e9e53
Derrik <infalen@gmail.com>
2016-10-25 13:31:37 -0500

    Changed Campbell URL, removed Auth and Game lobbies.

--- src/main/java/savemgo/nomad/Nomad.java
+++ src/main/java/savemgo/nomad/Nomad.java
@@ -5,35 +5,31 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import savemgo.nomad.campbell.Campbell;
 import savemgo.nomad.lobby.GateLobby;
-import savemgo.nomad.server.Lobby;
 import savemgo.nomad.server.NomadServer;
 
 public class Nomad {
 
-	private NomadServer serverGate, serverAuth, serverGame;
+	private NomadServer serverGate;
 
 	private EventLoopGroup bossGroup, workerGroup;
 
 	public Nomad() {
 		Campbell campbell = Campbell.instance();
-		campbell.setBaseUrl("https://api.savemgo.com/campbell/");
+		campbell.setBaseUrl("http://api.savemgo.com/campbell/");
 		campbell.setApiKey("ASecretKey");
 
 		try {
 			bossGroup = new NioEventLoopGroup();
 			workerGroup = new NioEventLoopGroup();
 
-			Lobby lobby = new GateLobby();
-			serverGate = new NomadServer(lobby, "0.0.0.0", 5731, bossGroup, workerGroup);
-			serverAuth = new NomadServer(lobby, "0.0.0.0", 5732, bossGroup, workerGroup);
-			serverGame = new NomadServer(lobby, "0.0.0.0", 5733, bossGroup, workerGroup);
+			serverGate = new NomadServer(new GateLobby(), "0.0.0.0", 5731, bossGroup, workerGroup);
 
 			System.out.println("Starting...");
 
 			serverGate.start();
-			serverAuth.start();
-			serverGame.start();
 
+			System.out.println("Started.");
+			
 			ChannelFuture future = serverGate.getFuture();
 
 			try {
